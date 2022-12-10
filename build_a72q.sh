b0d79bd4eed21a8c72c70f787a0c61af345836c5
Simon1511 <simon2002.schoenmackers@gmail.com>
2022-12-10 15:21:35 +0100

    arm64: Add A72 support
    Change-Id: I04d9f699ed6b0961bba20bd8cf8dd5602bea2475

---
+++ build_a72q.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PATH="/home/simon/lineage-19.1/prebuilts/clang/host/linux-x86/clang-r383902/bin:/home/simon/lineage-19.1/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:/home/simon/lineage-19.1/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:${PATH}"
+
+export ARCH=arm64
+
+make ARCH=arm64 vendor/a72q_eur_open_defconfig
+
+make ARCH=arm64 -j16 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
