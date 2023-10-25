8780ea08a16b77e25dd2d903dea2e4006a6647bb
Simon1511 <me@simon1511.de>
2023-10-25 21:33:25 +0200

    Use seperate out dir
    Change-Id: I24a4f8c29a716d7cbe808b75c77aa4746ce84206

--- build_a52q.sh
+++ build_a52q.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PATH="../toolchains/clang/clang-r383902/bin:../toolchains/gcc/aarch64/bin:../toolchains/gcc/arm/bin:${PATH}"
+PATH="../../toolchains/clang/clang-r383902/bin:../../toolchains/gcc/aarch64/bin:../../toolchains/gcc/arm/bin:${PATH}"
 
 export ARCH=arm64
 
-make ARCH=arm64 vendor/a52q_eur_open_defconfig
+make ARCH=arm64 O=out vendor/a52q_eur_open_defconfig
 
-make ARCH=arm64 -j16 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
+make ARCH=arm64 O=out -j16 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
