70b8485d0dbdd05f6f3dbe91db974c06b6fc94d8
Tim Zimmermann <tim@linux4.de>
2024-04-02 20:04:30 +0200

    input: Sync switch event codes with upstream linux
    * They are checked as of https://android.googlesource.com/platform/frameworks/native/+/9762ac9c1b813850379f55dfc2bd6698b8c96c64,
      values > upstream linux SW_MAX will now cause a system_server crash
    
    Change-Id: Ib26f230bfa663f78ec17fc91890679210558ad60

--- include/sound/jack.h
+++ include/sound/jack.h
@@ -106,7 +106,7 @@ enum snd_jack_types {
 #endif
 
 /* Keep in sync with definitions above */
-#define SND_JACK_SWITCH_TYPES 7
+#define SND_JACK_SWITCH_TYPES 6
 
 struct snd_jack {
 	struct list_head kctl_list;
