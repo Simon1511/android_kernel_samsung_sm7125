3a14797202809fb76214f694b76f3805556300eb
Simon1511 <me@simon1511.de>
2024-04-03 21:05:32 +0200

    sm7125: input: stm: force enable hover proximity sensor
    The sensors HAL passes ear_detect_enable,1 to /sys/class/sec/tsp/cmd, which leads to the proximity sensor being enabled.
    
    So far it seems like 1 enables touch proximity, whereas 3 enables hover proximity. It is yet to figure out what 2 does, but for now force enable the hover feature by altering the value passed by the HAL to 3.
    Also only force hovering when the screen is on because turning on the hover feature breaks proximity when in LPM.
    
    Additionally, devices with optical FOD will also need to reset the fod_pressed sysfs to 0 because "ear_detect_enable,3" prevents the driver from running into FTS_SPONGE_EVENT_GESTURE_ID_FOD_RELEASE, leaving fod_pressed at 1 all the time.
    
    Change-Id: I64a390dee18aa805c4ca968282168b906d5efa63
    Signed-off-by: Simon1511 <me@simon1511.de>

--- drivers/input/touchscreen/stm/fts5cu56a/fts_sec.c
+++ drivers/input/touchscreen/stm/fts5cu56a/fts_sec.c
@@ -7445,7 +7445,11 @@ static void ear_detect_enable(void *device_data)
 		snprintf(buff, sizeof(buff), "NG");
 		sec->cmd_state = SEC_CMD_STATUS_FAIL;
 	} else {
-		info->ed_enable = sec->cmd_param[0];
+		if (info->fts_power_state == FTS_POWER_STATE_LOWPOWER || info->fod_pressed) {
+			info->ed_enable = sec->cmd_param[0];
+			info->fod_pressed = 0;
+		} else
+			info->ed_enable = sec->cmd_param[0] != 0 ? 3 : 0;
 		snprintf(buff, sizeof(buff), "OK");
 
 		data[0] = FTS_CMD_SET_EAR_DETECT;
