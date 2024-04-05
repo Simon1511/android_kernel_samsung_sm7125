3c98cbb9bb70c7a6a7c8f7eaaa667c645e714597
Simon1511 <me@simon1511.de>
2024-04-05 12:27:42 +0200

    bla
    Change-Id: I943328edf0e946a988865edeb037968a45fe947f

--- drivers/gpu/drm/msm/samsung_lego/ss_dsi_panel_common.c
+++ drivers/gpu/drm/msm/samsung_lego/ss_dsi_panel_common.c
@@ -2997,6 +2997,12 @@ int ss_panel_off_post(struct samsung_display_driver_data *vdd)
 
 	LCD_INFO("[DISPLAY_%d] +\n", vdd->ndx);
 
+	if (vdd->finger_mask)
+		vdd->finger_mask = 0;
+	
+	if (vdd->br_info.common_br.finger_mask_bl_level)
+		vdd->br_info.common_br.finger_mask_bl_level = 0;
+
 	if (vdd->mdnie.support_trans_dimming)
 		vdd->mdnie.disable_trans_dimming = true;
 
